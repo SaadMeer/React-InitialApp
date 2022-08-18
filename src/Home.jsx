370e4b1d98a112b62d743d0f14b2894b79227cd5
Saad meer <saad.wicks@gmail.com>
2022-08-18 20:21:19 +0500

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <section id='header' className=''>
+                <div className='container-fluid nav-bg'>
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className='col-md-6 pt-lg-0 order-2 order-lg-1'>
+                                <h1>Grow Your Business With <strong>GetFood</strong></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
