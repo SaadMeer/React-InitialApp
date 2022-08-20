1134e8d939003237dece5343647e97071368fde6
Saad meer <saad.wicks@gmail.com>
2022-08-20 22:18:27 +0500

    second commit

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit 7c49cc8076faea9c152642295fe5e446788c6bab[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Saad meer <saad.wicks@gmail.com>
+Date:   Thu Aug 18 23:19:20 2022 +0500
+
+    second commit
+
+[33mcommit 50a2bc067bbe943ddae42e9def3296233ea257f5[m
+Author: Saad meer <saad.wicks@gmail.com>
+Date:   Thu Aug 18 23:14:17 2022 +0500
+
+    second commit
+
+[33mcommit 370e4b1d98a112b62d743d0f14b2894b79227cd5[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Saad meer <saad.wicks@gmail.com>
+Date:   Thu Aug 18 20:21:19 2022 +0500
+
+    first commit
+
+[33mcommit 058b5a9bcb746a5a3036b70e4bb1100c40896c30[m
+Author: Saad meer <saad.wicks@gmail.com>
+Date:   Wed Aug 17 21:15:44 2022 +0500
+
+    Initialize project using Create React App
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+const formSubmit = () => {
+    
+}
 
 const Contact = () => {
+    const [data, setdata] = useState({
+        fullname:'',
+        phone:'',
+        email:'',
+        msg:'',
+    });
+
+    const InputEvent = (event) => {
+        const {name, value} = event.target;
+        setdata((preVal) => {
+            return{
+                ...preVal,
+                [name]: value,
+            };
+        });
+    };
+
     return (
-        <div>
-            <h1>Welcome Contact Page</h1>
-        </div>
+        <>
+            <div className='my-5'>
+                <h1 className='text-center'>Contact US</h1>
+            </div>
+            <div className="conatiner contact_div">
+                <div className="row">
+                    <div className="col-md-6 col-10 mx-auto">
+                        <form onSubmit={formSubmit}>
+                            <div class="mb-3">
+                                <label class="form-label">Full Name</label>
+                                <input type="text " name="fullname" value={data.fullname} onClick={InputEvent} placeholder='enter your name' class="form-control" />
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label class="form-label">Phone Number</label>
+                                <input type="email" name="phone" value={data.phone} onChange={InputEvent} placeholder='phone number' class="form-control" />
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email address</label>
+                                <input type="email" name="email" value={data.email} onChange={InputEvent} placeholder='enter your email address' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Password</label>
+                                <input type="password" name="password"  onChange={InputEvent} placeholder='password' class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                                <textarea class="form-control" placeholder='message' name="msg" value={data.msg} onChange={InputEvent} id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" class="btn btn-outline-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
